df7e7868884a6909d97eb54cce915d1e04166ba1
josetornante <209896672+josetornante@users.noreply.github.com>
2025-06-11 12:51:55 -0400

    update randomvirel

--- randomvirel_test.go
+++ randomvirel_test.go
@@ -16,27 +16,27 @@ var tests = [][3][]byte{
 	{
 		[]byte("test key 000"),
 		[]byte("This is a test"),
-		hexDec("80cf38cee556e27b1a304b2bdc1aa3b93a6f2e3ed1a6a0f2b4e7fbd3f2270a75"),
+		hexDec("a90e9e2a6bb74a0e3a23da303805c36a150a67d293f8fe8bc6f982d7b445568f"),
 	},
 	{
 		[]byte("test key 000"),
 		[]byte("Lorem ipsum dolor sit amet"),
-		hexDec("fb622344fd64e7a2f93b6b660b97d1e57668075a42a329fc5e302a3122098dbb"),
+		hexDec("eb0aad09fcefb60bb991d2459a97c5098b0d9e8def7033400a46884864890064"),
 	},
 	{
 		[]byte("test key 000"),
 		[]byte("sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"),
-		hexDec("2534becac5c7594708d435b231e8c3482bf46679a92d3124a59ce82335fc0205"),
+		hexDec("e4e48f5fe3bb58d9f8e07f8d130bb4e441b98e7102751400437551426e3f337c"),
 	},
 	{
 		[]byte("test key 001"),
 		[]byte("sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"),
-		hexDec("66759fa6969545fb3dcc6a6f124cb837c81bff6364d7f7e0af822a4f764b15e9"),
+		hexDec("56052c254fc0e0dfbc80cb38fb6cb47872e52cd68048442f9e539e49116202c0"),
 	},
 	{
 		[]byte("test key 001"),
 		hexDec("0b0b98bea7e805e0010a2126d287a2a0cc833d312cb786385a7c2f9de69d25537f584a9bc9977b00000000666fd8753bf61a8631f12984e3fd44f4014eca629276817b56f32e9b68bd82f416"),
-		hexDec("3bbb57a343c6e504adc5afd1673c2d595cbbe33512657f06079eecc292482019"),
+		hexDec("6c6e908fbb14c3d60126221cc4e1ced7a9d812c565efdd761f52e98f8dbe15e4"),
 	},
 }
 
@@ -69,11 +69,7 @@ func TestMain(t *testing.T) {
 	for _, tp := range tests {
 		hash := PowHashArbitrarySeed(tp[0], tp[1])
 
-		var hashCorrect = make([]byte, hex.DecodedLen(len(tp[2])))
-		_, err := hex.Decode(hashCorrect, tp[2])
-		if err != nil {
-			t.Log(err)
-		}
+		var hashCorrect = tp[2]
 
 		if !bytes.Equal(hash[:], hashCorrect) {
 			t.Fatalf("full mode: incorrect hash: expected %x, got %x", hashCorrect, hash)
